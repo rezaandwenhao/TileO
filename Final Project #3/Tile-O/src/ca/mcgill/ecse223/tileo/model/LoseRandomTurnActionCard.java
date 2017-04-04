032703de13ac98863ec2f0b180ac18d89bf3bc19
Jiawei Ni <jiaweini@live.com>
2017-04-04 01:34:03 -0400

    ac

---
+++ Final Project #3/Tile-O/src/ca/mcgill/ecse223/tileo/model/LoseRandomTurnActionCard.java
@@ -0,0 +1,40 @@
+/*PLEASE DO NOT EDIT THIS CODE*/
+/*This code was generated using the UMPLE 1.25.0-9e8af9e modeling language!*/
+
+package ca.mcgill.ecse223.tileo.model;
+
+// line 88 "../../../../../TileO (updated Feb10).ump"
+public class LoseRandomTurnActionCard extends ActionCard
+{
+
+  //------------------------
+  // MEMBER VARIABLES
+  //------------------------
+
+  //------------------------
+  // CONSTRUCTOR
+  //------------------------
+
+  public LoseRandomTurnActionCard(String aInstructions, Deck aDeck)
+  {
+    super(aInstructions, aDeck);
+  }
+
+  public LoseRanNumTurn(){
+	  lk;
+	  
+	  
+	  
+  }
+  
+  
+  //------------------------
+  // INTERFACE
+  //------------------------
+
+  public void delete()
+  {
+    super.delete();
+  }
+
+}
