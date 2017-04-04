37b307125217673557907a6710c0fa538fd511d5
jiaweini <jiaweini@users.noreply.github.com>
2017-04-04 01:36:37 -0400

    Add files via upload

---
+++ Final Project #3/Tile-O/src/ca/mcgill/ecse223/tileo/model/LoseRandomTurnActionCard.java
@@ -0,0 +1,45 @@
+/*PLEASE DO NOT EDIT THIS CODE*/
+/*This code was generated using the UMPLE 1.25.0-9e8af9e modeling language!*/
+
+package ca.mcgill.ecse223.tileo.model;
+
+import java.util.List;
+import java.util.Random;
+
+// line 88 "../../../../../TileO (updated Feb10).ump"
+public class LoseRandomTurnActionCard extends ActionCard
+{
+
+  //------------------------
+  // MEMBER VARIABLES
+  //------------------------
+
+  //------------------------
+  // CONSTRUCTOR
+  //------------------------
+
+  public LoseRandomTurnActionCard(String aInstructions, Deck aDeck)
+  {
+    super(aInstructions, aDeck);
+  }
+
+  public void LoseRanNumTurn(List<Player> players){
+	  
+	for(Player player: players){
+		int  turnLost = (int)(Math.random()*2);
+		player.setTurnsUntilActive(player.getTurnsUntilActive()+turnLost);
+	}
+	  
+  }
+  //=--=-=-=-=
+  
+  //------------------------
+  // INTERFACE
+  //------------------------
+
+  public void delete()
+  {
+    super.delete();
+  }
+
+}
